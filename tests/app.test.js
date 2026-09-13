6cda72836a22c21774a43371aecf8933ad538788
Che140401 <karandechetan106@gmail.com>
2026-09-13 18:09:10 +0530

    first commit of project

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return Hello message', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toBe('Hello, Github Actions!');
+    expect(res.statusCode).toBe(200);
+  });
+});
