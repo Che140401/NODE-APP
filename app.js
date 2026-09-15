724f1f4ddf84c236022a60a080488639a3d14ea2
Che140401 <karandechetan106@gmail.com>
2026-09-15 20:38:05 +0530

    app modified

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Github Actions!');
+  res.send('Hello, Github Actions! Welcome back');
 });
 
 const PORT = process.env.PORT || 3000;
--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,7 @@ const app = require('../app');
 describe('GET /', () => {
   it('should return Hello message', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('Hello, Github Actions!');
+    expect(res.text).toBe('Hello, Github Actions! Welcome back');
     expect(res.statusCode).toBe(200);
   });
 });
